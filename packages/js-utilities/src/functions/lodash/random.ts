7a1cdb7a503bc2a90edfeb92ca4a08da8b87b592
João Dias <contacto@joaodias.me>
2024-02-25 20:07:24 +0000

    feat(lodash): added draw function for getting random items from lists.

--- packages/js-utilities/src/functions/lodash/random.ts
+++ packages/js-utilities/src/functions/lodash/random.ts
@@ -1,4 +1,4 @@
-import { isBoolean, isUndefined } from "./typed";
+import { isArray, isBoolean, isNil, isUndefined } from "./typed";
 
 /**
  * Produces a random number between the inclusive lower and upper bounds.
@@ -29,22 +29,52 @@ import { isBoolean, isUndefined } from "./typed";
  * @param {number} [lower=0]
  * @param {(number | boolean)} [upper]
  * @param {boolean} [floating]
- * @returns {*}  {number}
+ * @returns {number} a random number
  */
 export function random(lower: number = 0, upper?: number | boolean, floating?: boolean): number {
-  if (isBoolean(upper)) {
-    floating = upper;
-    upper = undefined;
-  }
+	if (isBoolean(upper)) {
+		floating = upper;
+		upper = undefined;
+	}
 
-  if (isNaN(upper as number)) {
-    upper = lower < 0 ? 0 : 1;
-  }
+	if (isNaN(upper as number)) {
+		upper = lower < 0 ? 0 : 1;
+	}
 
-  if (isUndefined(floating)) {
-    floating = !Number.isInteger(lower) || !Number.isInteger(upper as number);
-  }
+	if (isUndefined(floating)) {
+		floating = !Number.isInteger(lower) || !Number.isInteger(upper as number);
+	}
 
-  const randomNumber = Math.random() * ((upper as number) - lower) + lower;
-  return floating ? randomNumber : Math.round(randomNumber);
+	const randomNumber = Math.random() * ((upper as number) - lower) + lower;
+	return floating ? randomNumber : Math.round(randomNumber);
+}
+
+/**
+ * Draw a random item from a list. Returns null if the list is empty.
+ *
+ * This helper can also be useful for using inside tests where - given a list of possible outcomes - one is randomly selected.
+ *
+ * @example
+ *
+ * const characters = [
+ * 	"Homer Simpson",
+ * 	"Marge Simpson",
+ * 	"Bart Simpson",
+ * 	"Lisa Simpson",
+ * 	"Maggie Simpson",
+ * ];
+ *
+ * draw(characters)
+ * // => a random Simpsons Character e.g "Bart Simpson"
+ */
+export function draw<T>(array: readonly T[]): T | null {
+	const UPPER_VALUE = !isNil(array) && isArray(array) ? array.length : 0;
+
+	if (UPPER_VALUE === 0) {
+		return null;
+	}
+
+	const index = random(0, UPPER_VALUE - 1);
+
+	return array[index];
 }
