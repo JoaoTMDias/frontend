1d9a19d54403806ae5bf92b3383f1ea542c1e361
João Dias <joao.dias@feedzai.com>
2025-01-18 23:50:47 +0000

    tests: added more test coverage

--- packages/js-utilities/cypress.config.ts
+++ packages/js-utilities/cypress.config.ts
@@ -1,28 +1,33 @@
+/**
+ * Please refer to the terms of the license agreement in the root of the project
+ *
+ * (c) 2024 joaodias.me
+ */
 import { defineConfig } from "cypress";
 import codeCoverageTask from "@cypress/code-coverage/task";
 
 export default defineConfig({
-  env: {
-    codeCoverage: {
-      exclude: "cypress/**/*.*",
-    },
-    TZ: "Europe/Lisbon",
-  },
-  component: {
-    retries: {
-      runMode: 2,
-    },
-    video: false,
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-    viewportWidth: 800,
-    viewportHeight: 800,
-    setupNodeEvents(on, config) {
-      codeCoverageTask(on, config);
+	env: {
+		codeCoverage: {
+			exclude: "cypress/**/*.*",
+		},
+		TZ: "Europe/Lisbon",
+	},
+	component: {
+		retries: {
+			runMode: 2,
+		},
+		video: false,
+		devServer: {
+			framework: "react",
+			bundler: "vite",
+		},
+		viewportWidth: 800,
+		viewportHeight: 800,
+		setupNodeEvents(on, config) {
+			codeCoverageTask(on, config);
 
-      return config;
-    },
-  },
+			return config;
+		},
+	},
 });
